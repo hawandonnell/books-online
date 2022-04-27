a2635d5254fed10224a495a48df43c19c0f0d4b5
Altair <tair_ermekbai@mail.ru>
2022-04-27 20:28:07 +0600

    Fix books stars script

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,48 @@
-const gulp = require('gulp')
-const postcss = require('gulp-postcss')
-const sourcemaps = require('gulp-sourcemaps')
-const uglify = require('gulp-uglify')
+const gulp = require("gulp");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
+const uglify = require("gulp-uglify");
 // const cleanCss = require('gulp-clean-css')
 
-
 function css() {
-    return gulp.src('./css/*.css')
-        .pipe(sourcemaps.init())
-        .pipe(postcss([ require('autoprefixer'), require('cssnano')({
-            preset: ['default', {
-                discardDuplicates: true,
-                uniqueSelectors: true,
-                cssDeclarationSorter: true,
-                mergeRules: true,
-                mergeLonghand: true,
-                minifyFontValues: true
-            }]
-        }) ]))
-        // .pipe(cleanCss())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('build/'))
+	return (
+		gulp
+			.src("./css/*.css")
+			.pipe(sourcemaps.init())
+			.pipe(
+				postcss([
+					require("autoprefixer"),
+					require("cssnano")({
+						preset: [
+							"default",
+							{
+								discardDuplicates: true,
+								uniqueSelectors: true,
+								cssDeclarationSorter: true,
+								mergeRules: true,
+								mergeLonghand: true,
+								minifyFontValues: true,
+							},
+						],
+					}),
+				])
+			)
+			// .pipe(cleanCss())
+			.pipe(sourcemaps.write("."))
+			.pipe(gulp.dest("build/"))
+	);
 }
 
 function js() {
-    return gulp.src('./script.js')
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('build/'))
+	return gulp
+		.src("./script.js")
+		.pipe(sourcemaps.init())
+		.pipe(uglify())
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest("build/"));
 }
-
 
 exports.default = () => {
-    gulp.watch('./css/*.css', css)
-    gulp.watch('./script.js', js)
-}
+	gulp.watch("./css/*.css", css);
+	gulp.watch("./script.js", js);
+};
--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-const star = document.getElementsByClassName('svg-star')
-for (var i = 0; i < star.length; i++) {
-    star[i].addEventListener('mouseover', (e) => {
-        const parentArr = e.target.parentElement.children
-        for (var j = 0; j <= Array.from(parentArr).indexOf(e.target); j++) {
-            parentArr[j].style.fill = "#FF0000"
-        }
-    })
-    star[i].addEventListener('mouseout', (e) => {
-        const parentArr = e.target.parentElement.children
-        for (var j = 0; j <= Array.from(parentArr).indexOf(e.target); j++) {
-            parentArr[j].style.fill = ""
-        }
-    })
+const star = document.getElementsByClassName("svg-star");
+for (let i = 0; i < star.length; i++) {
+	star[i].addEventListener("mouseover", (e) => {
+		let parentArr = e.target.parentElement.children;
+		for (let j = 0; j <= Array.from(parentArr).indexOf(e.target); j++) {
+			parentArr[j].style.fill = "#FF0000";
+		}
+	});
+	star[i].addEventListener("mouseout", (e) => {
+		let parentArr = e.target.parentElement.children;
+		for (let j = 0; j <= Array.from(parentArr).indexOf(e.target); j++) {
+			parentArr[j].style.fill = "";
+		}
+	});
 }
-
